e0825a1bde3ba6508d0cdb4ec689970df21d7bcd
david-puente <43456778+david-puente@users.noreply.github.com>
2020-03-27 01:46:31 +0100

    Deshace el último commit

--- src/Normalizer/FileEntityNormalizer.php
+++ src/Normalizer/FileEntityNormalizer.php
@@ -2,83 +2,112 @@
 
 namespace Drupal\content_sync\Normalizer;
 
-use Drupal\content_sync\ContentSyncManager;
 use Drupal\content_sync\Plugin\SyncNormalizerDecoratorManager;
-use Drupal\content_sync\Plugin\SyncNormalizerDecoratorTrait;
-use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Entity\RevisionableInterface;
-use Drupal\Core\Url;
-use Drupal\serialization\Normalizer\ContentEntityNormalizer as BaseContentEntityNormalizer;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
 
 /**
  * Adds the file URI to embedded file entities.
  */
-class ContentEntityNormalizer extends BaseContentEntityNormalizer {
-
-  use SyncNormalizerDecoratorTrait;
+class FileEntityNormalizer extends ContentEntityNormalizer {
 
   /**
-   * @var SyncNormalizerDecoratorManager
-   */
-  protected $decoratorManager;
-
-  /**
-   * Constructs an EntityNormalizer object.
+   * The interface or class that this Normalizer supports.
    *
-   * @param EntityTypeManagerInterface $entity_manager
+   * @var string
+   */
+  protected $supportedInterfaceOrClass = 'Drupal\file\FileInterface';
+
+  /**
+   * File system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * FileEntityNormalizer constructor.
+   *
+   * @param EntityManagerInterface $entity_manager
    *
    * @param SyncNormalizerDecoratorManager $decorator_manager
+   *
+   * @param FileSystemInterface $file_system
    */
-  public function __construct(EntityTypeManagerInterface $entity_manager, SyncNormalizerDecoratorManager $decorator_manager) {
-    parent::__construct($entity_manager);
-    $this->decoratorManager = $decorator_manager;
+  public function __construct(EntityManagerInterface $entity_manager, SyncNormalizerDecoratorManager $decorator_manager, FileSystemInterface $file_system) {
+    parent::__construct($entity_manager, $decorator_manager);
+    $this->fileSystem = $file_system;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function denormalize($data, $class, $format = NULL, array $context = []) {
-    if (is_null($data)) {
-      return NULL;
-    }
-    $original_data = $data;
+  public function denormalize($data, $class, $format = NULL, array $serializer_context = array()) {
 
-    // Get the entity type ID while letting context override the $class param.
-    $entity_type_id = !empty($context['entity_type']) ? $context['entity_type'] : $this->entityTypeManager->getEntityTypeFromClass($class);
+    $file_data = '';
 
-    $bundle = FALSE;
-    /** @var \Drupal\Core\Entity\EntityTypeInterface $entity_type_definition */
-    // Get the entity type definition.
-    $entity_type_definition = $this->entityTypeManager->getDefinition($entity_type_id, FALSE);
-    if ($entity_type_definition->hasKey('bundle')) {
-      $bundle_key = $entity_type_definition->getKey('bundle');
-      // Get the base field definitions for this entity type.
-      $base_field_definitions = $this->entityTypeManager->getBaseFieldDefinitions($entity_type_id);
-
-      // Get the ID key from the base field definition for the bundle key or
-      // default to 'value'.
-      $key_id = isset($base_field_definitions[$bundle_key]) ? $base_field_definitions[$bundle_key]->getFieldStorageDefinition()
-        ->getMainPropertyName() : 'value';
-
-      // Normalize the bundle if it is not explicitly set.
-      $bundle = isset($data[$bundle_key][0][$key_id]) ? $data[$bundle_key][0][$key_id] : (isset($data[$bundle_key]) ? $data[$bundle_key] : NULL);
+    // Check if the image is available as base64-encoded image.
+    if (!empty($data['data'][0]['value'])) {
+      $file_data = $data['data'][0]['value'];
+      // Avoid 'data' being treated as a field.
+      unset($data['data']);
     }
 
-    // Decorate data before denormalizing it.
-    $this->decorateDenormalization($data, $entity_type_id, $format, $context);
+    // If a directory is set, we must to copy the file to the file system.
+    if (!empty($serializer_context['content_sync_directory_files'])) {
+      $scheme = $this->fileSystem->uriScheme($data['uri'][0]['value']);
+      if (!empty($scheme)) {
+        $source_path = realpath($serializer_context['content_sync_directory_files']) . '/' .$scheme . '/';
+        $source      = str_replace($scheme . '://', $source_path, $data['uri'][0]['value']);
+        if (file_exists($source)) {
+          $file = $this->fileSystem->realpath($data['uri'][0]['value']);
+          if (!file_exists($file) || (md5_file($file) !== md5_file($source))) {
+            $dir = $this->fileSystem->dirname($data['uri'][0]['value']);
+            file_prepare_directory($dir, FILE_CREATE_DIRECTORY);
+            $uri = file_unmanaged_copy($source, $data['uri'][0]['value']);
+            $data['uri'] = [
+              [
+                'value' => $uri,
+                'url' => str_replace($GLOBALS['base_url'], '', file_create_url($uri))
+              ]
+            ];
 
-    // Resolve references
-    $this->fixReferences($data, $entity_type_id, $bundle);
+            // We just need one method to create the image.
+            $file_data = '';
+          }
+        }
+      }
+    }
 
-    // Remove invalid fields
-    $this->cleanupData($data, $entity_type_id, $bundle);
+    $entity = parent::denormalize($data, $class, $format, $serializer_context);
 
-    // Data to Entity
-    $entity = parent::denormalize($data, $class, $format, $context);
+    // If the image was sent as base64 we must to create the physical file.
+    if ($file_data) {
+      // Decode and save to file.
+      $file_contents = base64_decode($file_data);
+      $dirname = $this->fileSystem->dirname($entity->getFileUri());
+      file_prepare_directory($dirname, FILE_CREATE_DIRECTORY);
+      if ($uri = file_unmanaged_save_data($file_contents, $entity->getFileUri())) {
+        $entity->setFileUri($uri);
+      }
+      else {
+        throw new \RuntimeException(SafeMarkup::format('Failed to write @filename.', array('@filename' => $entity->getFilename())));
+      }
+    }
 
-    // Decorate denormalized entity before retuning it.
-    $this->decorateDenormalizedEntity($entity, $original_data, $format, $context);
+    // If the image was sent as URL we must to create the physical file.
+    /*if ($file_data) {
+      // Decode and save to file.
+      $file_contents = base64_decode($file_data);
+      $dirname = $this->fileSystem->dirname($entity->getFileUri());
+      file_prepare_directory($dirname, FILE_CREATE_DIRECTORY);
+      if ($uri = file_unmanaged_save_data($file_contents, $entity->getFileUri())) {
+        $entity->setFileUri($uri);
+      }
+      else {
+        throw new \RuntimeException(SafeMarkup::format('Failed to write @filename.', array('@filename' => $entity->getFilename())));
+      }
+    }*/
 
     return $entity;
   }
@@ -86,207 +115,26 @@ class ContentEntityNormalizer extends BaseContentEntityNormalizer {
   /**
    * {@inheritdoc}
    */
-  public function normalize($object, $format = NULL, array $context = []) {
-    /* @var ContentEntityInterface $object */
-    $normalized_data = parent::normalize($object, $format, $context);
+  public function normalize($object, $format = NULL, array $serializer_context = array()) {
+    $data = parent::normalize($object, $format, $serializer_context);
 
-    $normalized_data['_content_sync'] = $this->getContentSyncMetadata($object, $context);
-
-    /**
-     * @var \Drupal\Core\Entity\ContentEntityBase $object
-     */
-    $referenced_entities = $object->referencedEntities();
-
-    // Add node uuid for menu link if any.
-    if ($object->getEntityTypeId() == 'menu_link_content') {
-      if ($entity = $this->getMenuLinkNodeAttached($object)) {
-        $normalized_data['_content_sync']['menu_entity_link'][$entity->getEntityTypeId()] = $entity->uuid();
-        $referenced_entities[] = $entity;
-      }
+    // The image will be saved in the export directory.
+    if (!empty($serializer_context['content_sync_directory_files'])) {
+      $uri = $object->getFileUri();
+      $scheme = $this->fileSystem->uriScheme($uri);
+      $destination = "{$serializer_context['content_sync_directory_files']}/{$scheme}/";
+      $destination = str_replace($scheme . '://', $destination, $uri);
+      file_prepare_directory($this->fileSystem->dirname($destination), FILE_CREATE_DIRECTORY);
+      file_unmanaged_copy($uri, $destination, FILE_EXISTS_REPLACE);
     }
 
-    if (!empty($referenced_entities)) {
-      $dependencies = [];
-      foreach ($referenced_entities as $entity) {
-        $reflection = new \ReflectionClass($entity);
-        if ($reflection->implementsInterface(ContentEntityInterface::class)) {
-          $ids = [
-            $entity->getEntityTypeId(),
-            $entity->bundle(),
-            $entity->uuid(),
-          ];
-          $dependency = implode(ContentSyncManager::DELIMITER, $ids);
-          if (!$this->inDependencies($dependency, $dependencies)) {
-            $dependencies[$entity->getEntityTypeId()][] = $dependency;
-          }
-        }
-      }
-      $normalized_data['_content_sync']['entity_dependencies'] = $dependencies;
+    // Set base64-encoded file contents to the "data" property.
+    if (!empty($serializer_context['content_sync_file_base_64'])) {
+      $file_data = base64_encode(file_get_contents($object->getFileUri()));
+      $data['data'] = [['value' => $file_data]];
     }
-    // Decorate normalized entity before retuning it.
-    if (is_a($object, ContentEntityInterface::class, TRUE)) {
-      $this->decorateNormalization($normalized_data, $object, $format, $context);
-    }
-    return $normalized_data;
-  }
 
-  /**
-   * Checks if a dependency is in a dependencies nested array.
-   *
-   * @param string $dependency
-   *   An entity identifier.
-   * @param $dependencies
-   *   A nested array of dependencies.
-   *
-   * @return bool
-   */
-  protected function inDependencies($dependency, $dependencies) {
-    list($entity_type_id, $bundle, $uuid) = explode('.', $dependency);
-    if (isset($dependencies[$entity_type_id])) {
-      if (in_array($dependency, $dependencies[$entity_type_id])) return TRUE;
-    }
-    return FALSE;
-  }
-
-  /**
-   * Gets a node attached to a menu link. The node has already been imported.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $object
-   *   Menu Link Entity
-   *
-   * @return bool|\Drupal\Core\Entity\EntityInterface|null
-   *   Node Entity.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   */
-  protected function getMenuLinkNodeAttached($object) {
-    $entity = FALSE;
-
-    $uri = $object->get('link')->getString();
-    $url = Url::fromUri($uri);
-    $route_parameters = NULL;
-    try {
-      $route_parameters = $url->getRouteParameters();
-    }
-    catch (\Exception $e) {
-      // If menu link is linked to a non-node page - just do nothing.
-    }
-    if (count($route_parameters) == 1) {
-      $entity_id = reset($route_parameters);
-      $entity_type = key($route_parameters);
-      $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($entity_id);
-    }
-    return $entity;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  public function supportsNormalization($data, $format = NULL) {
-    return parent::supportsNormalization($data, $format) && !empty($data->is_content_sync);
-  }
-
-  /**
-   * @inheritdoc
-   */
-  public function supportsDenormalization($data, $type, $format = NULL) {
-    return parent::supportsDenormalization($data, $type, $format);
-  }
-
-  /**
-   * @param $object
-   * @param array $context
-   *
-   * @return array
-   */
-  protected function getContentSyncMetadata($object, $context = []) {
-    $metadata = [
-      'entity_type' => $object->getEntityTypeId(),
-    ];
-    return $metadata;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  protected function getDecoratorManager() {
-    return $this->decoratorManager;
-  }
-
-  /**
-   * @param array $data
-   * @param $entity_type_id
-   *
-   * @return array
-   */
-  protected function fixReferences(&$data, $entity_type_id, $bundle = FALSE) {
-    if ($bundle) {
-      $field_definitions = $this->entityTypeManager->getFieldDefinitions($entity_type_id, $bundle);
-    }
-    else {
-      $bundles = array_keys($this->entityTypeManager->getBundleInfo($entity_type_id));
-      $field_definitions = [];
-      foreach ($bundles as $bundle) {
-        $field_definitions_bundle = $this->entityTypeManager->getFieldDefinitions($entity_type_id, $bundle);
-        if (is_array($field_definitions_bundle)) {
-          $field_definitions += $field_definitions_bundle;
-        }
-      }
-    }
-    foreach ($field_definitions as $field_name => $field_definition) {
-      // We are only interested in importing content entities.
-      if (!is_a($field_definition->getClass(), '\Drupal\Core\Field\EntityReferenceFieldItemList', TRUE)) {
-        continue;
-      }
-      if (!empty($data[$field_name]) && is_array($data[$field_name])) {
-        $key = $field_definition->getFieldStorageDefinition()
-          ->getMainPropertyName();
-        foreach ($data[$field_name] as $i => &$item) {
-          if (!empty($item['target_uuid'])) {
-            $reference = $this->entityTypeManager->loadEntityByUuid($item['target_type'], $item['target_uuid']);
-            if ($reference) {
-              $item[$key] = $reference->id();
-              if (is_a($reference, RevisionableInterface::class, TRUE)) {
-                $item['target_revision_id'] = $reference->getRevisionId();
-              }
-            }
-            else {
-              $reflection = new \ReflectionClass($this->entityTypeManager->getStorage($item['target_type'])->getEntityType()->getClass());
-              if ($reflection->implementsInterface(ContentEntityInterface::class)) {
-                unset($data[$field_name][$i]);
-              }
-            }
-          }
-        }
-      }
-    }
     return $data;
-  }
-
-  /**
-   * @param $data
-   * @param $entity_type_id
-   */
-  protected function cleanupData(&$data, $entity_type_id, $bundle = FALSE) {
-    if ($bundle) {
-      $field_definitions = $this->entityTypeManager->getFieldDefinitions($entity_type_id, $bundle);
-    }
-    else {
-      $bundles = array_keys($this->entityTypeManager->getBundleInfo($entity_type_id));
-      $field_definitions = [];
-      foreach ($bundles as $bundle) {
-        $field_definitions_bundle = $this->entityTypeManager->getFieldDefinitions($entity_type_id, $bundle);
-        if (is_array($field_definitions_bundle)) {
-          $field_definitions += $field_definitions_bundle;
-        }
-      }
-    }
-    $field_names = array_keys($field_definitions);
-    foreach ($data as $field_name => $field_data) {
-      if (!in_array($field_name, $field_names)) {
-        unset($data[$field_name]);
-      }
-    }
   }
 
 }
